9bea69807490186df162e65d67aa1077a14a7d7a
Rodrigo Martins <rodrigo.martins@wyda.com.br>
2020-01-07 11:04:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname +'/index.html')
+})
+
+io.on('connection', socket => {
+    console.log('a user connected')
+    socket.on('disconnect', () => {
+        console.log('a user disconnected')
+    })
+    socket.on('chat message', msg => {
+        console.log('message: ' + msg)
+        io.emit('chat message', msg)
+    })
+})
+
+http.listen(3000, console.log('Executando na porta 3000'))
